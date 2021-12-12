a0a21ffca56359ec1fe2777a6f9caabe7cc2ee8e
Jon-Anders Teigen <jteigen@gmail.com>
2021-12-12 23:02:10 +0100

    prettier (formatter for js/ts)

--- src/main/ts/App.tsx
+++ src/main/ts/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Col, Container, Form, Row, Spinner, Table } from "react-bootstrap";
+import React from 'react'
+import { Button, Col, Container, Form, Row, Spinner, Table } from 'react-bootstrap'
 
 interface CardStat {
   yellow: number
@@ -8,11 +8,11 @@ interface CardStat {
 }
 
 interface MatchStat {
-    fiksId: string
-    tidspunkt: string
-    home: string
-    away: string
-    cards: CardStat
+  fiksId: string
+  tidspunkt: string
+  home: string
+  away: string
+  cards: CardStat
 }
 
 interface RefereeStats {
@@ -24,21 +24,20 @@ interface RefereeStats {
 
 const App: React.VFC = () => {
   const [fetching, setFetching] = React.useState(false)
-  const [dommer, setDommer] = React.useState("")
+  const [dommer, setDommer] = React.useState('')
   const [refereeStats, setRefereeStats] = React.useState<RefereeStats>()
 
-  const fiksId = React.useMemo(() => { 
-    console.log('dommer', dommer)
-    if(dommer){
+  const fiksId = React.useMemo(() => {
+    if (dommer) {
       const url = new URL(dommer)
-      const search = new URLSearchParams(url.search)      
+      const search = new URLSearchParams(url.search)
       return search.get('fiksId')
     } else {
       return null
     }
   }, [dommer])
 
-  const hentStatistikk = async () => {        
+  const hentStatistikk = async () => {
     if (fiksId !== null) {
       try {
         setFetching(true)
@@ -59,46 +58,52 @@ const App: React.VFC = () => {
       <Form>
         <Row>
           <Col xs={7}>
-            <Form.Control type="input" value={dommer} onChange={(e) => setDommer(e.target.value) } placeholder="https://www.fotball.no/fotballdata/person/dommeroppdrag/?fiksId=xxxx"  />
+            <Form.Control
+              type="input"
+              value={dommer}
+              onChange={(e) => setDommer(e.target.value)}
+              placeholder="https://www.fotball.no/fotballdata/person/dommeroppdrag/?fiksId=xxxx"
+            />
           </Col>
           <Col xs={3}>
-          <Button variant="primary" onClick={() => hentStatistikk()} disabled={fiksId == null}>
-            {fetching && <Spinner as="span" animation="border" size="sm" />}
-            Hent statistikk
-          </Button>
+            <Button variant="primary" onClick={() => hentStatistikk()} disabled={fiksId == null}>
+              {fetching && <Spinner as="span" animation="border" size="sm" />}
+              Hent statistikk
+            </Button>
           </Col>
         </Row>
       </Form>
-      {refereeStats && <div>
-        <h4>{refereeStats.refereeName}</h4>
-        <Table>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Snitt</th>
-              <th>Totalt</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Gult</td>
-              <td>{refereeStats.averages.yellow.toFixed(2)}</td>
-              <td>{refereeStats.totals.yellow}</td>
-            </tr>
-            <tr>
-              <td>Gult nr. 2</td>
-              <td>{refereeStats.averages.yellowToRed.toFixed(2)}</td>
-              <td>{refereeStats.totals.yellowToRed}</td>
-            </tr>
-            <tr>
-              <td>Rødt</td>
-              <td>{refereeStats.averages.red.toFixed(2)}</td>
-              <td>{refereeStats.totals.red}</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-      }
+      {refereeStats && (
+        <div>
+          <h4>{refereeStats.refereeName}</h4>
+          <Table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Snitt</th>
+                <th>Totalt</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Gult</td>
+                <td>{refereeStats.averages.yellow.toFixed(2)}</td>
+                <td>{refereeStats.totals.yellow}</td>
+              </tr>
+              <tr>
+                <td>Gult nr. 2</td>
+                <td>{refereeStats.averages.yellowToRed.toFixed(2)}</td>
+                <td>{refereeStats.totals.yellowToRed}</td>
+              </tr>
+              <tr>
+                <td>Rødt</td>
+                <td>{refereeStats.averages.red.toFixed(2)}</td>
+                <td>{refereeStats.totals.red}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      )}
     </Container>
   )
 }
