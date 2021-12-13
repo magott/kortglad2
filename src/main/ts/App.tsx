a4d51337fcd234840d0d2069d3d9540da872d5f6
Morten Andersen-Gott <morten.andersengott@gmail.com>
2021-12-13 23:01:02 +0100

    Season greetings

--- src/main/ts/App.tsx
+++ src/main/ts/App.tsx
@@ -132,7 +132,7 @@ const App: React.VFC = () => {
               </tr>
               </thead>
               <tbody>
-              {refereeStats.matches.map((match) => (
+              {refereeStats.seasons[0].matches.map((match) => (
                   <tr>
                     <td>{match.tidspunkt.replace('T', ' ')}</td>
                     <td>{match.home}</td>
