4a1c570d6b5eafaec09774364ffa7b0f0ef11cfb
Jon-Anders Teigen <jteigen@gmail.com>
2021-12-14 19:50:27 +0100

    jsonb

--- src/main/scala/kortglad/Main.scala
+++ src/main/scala/kortglad/Main.scala
@@ -5,10 +5,35 @@ import bloque.db.transactional
 import bloque.jetty.*
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
+import bloque.db.Row
+import bloque.http.Json
+import java.sql.Types
+import java.sql.ResultSet
+import org.postgresql.util.PGobject
+
+given Row[Object] =
+  Row.jdbc(Types.OTHER, Nil, _.getObject(_), _.setObject(_, _))
+given Row[PGobject] =
+  Row[Object].imap(_.asInstanceOf[PGobject], identity)
+
+/*
+  object Thing:
+    given Row[Thing] = jsonb[Thing]
+  case class Thing(age:Int, name:String) derives Json
+ */
+
+def jsonb[A](using j: Json[A]): Row[A] =
+  def read(pg: PGobject) = Json.read(pg.getValue, false)
+  def write(a: A) =
+    val pg = new PGobject
+    pg.setType("jsonb")
+    pg.setValue(Json.default.write(a))
+    pg
+  Row[PGobject].imap(read, write)
 
 import scala.util.Using
 
-@main def main = Jetty(8080, "./public") {
+@main def main =
   Using.resource(HikariDataSource()) { ds =>
     ds.setUsername("postgres")
     ds.setPassword("postgres")
@@ -16,6 +41,7 @@ import scala.util.Using
     val tx = Db.fromDataSource(ds).transactional
     val flyway = Flyway.configure().dataSource(ds).load()
     flyway.migrate()
-    App.run(tx)
+    Jetty(8080, "./public") {
+      App.run(tx)
+    }
   }
-}
