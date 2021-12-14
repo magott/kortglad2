973f3850a48f2ed6779311d09ba7eb2a8da91678
Morten Andersen-Gott <morten.andersengott@gmail.com>
2021-12-14 20:48:11 +0100

    Meh db

--- src/main/scala/kortglad/RefereeDatabase.scala
+++ src/main/scala/kortglad/RefereeDatabase.scala
@@ -4,17 +4,17 @@ import bloque.db.Row
 import bloque.db.*
 
 import java.time.Year
-class RefereeDatabase {
 
-  case class DbReferee(fiksId: FiksId, name: String) derives Row
-  case class DbSeason(
-      year: Year,
-      matchStats: List[MatchStat]
-  )
-}
+case class DbReferee(fiksId: FiksId, name: String) derives Row
+case class DbSeason(year: Year, matchStats: List[MatchStat])
 
 def refereeById(fiksId: FiksId) =
   sql"select fiks_id, name from referee where fiks_id = ${fiksId.fiksId}"
+    .query[DbReferee]
 
-def refereeSeasonByRefereeId(fiksId: FiksId) =
-  sql"select referee_id, year, matches from referee_season where referee_id = ${fiksId.fiksId}"
+//def refereeSeasonByRefereeId(fiksId: FiksId) =
+//  sql"select referee_id, year, matches from referee_season where referee_id = ${fiksId.fiksId}"
+//    .query[DbSeason]
+
+def insertReferee(name: String, fiksId: FiksId) =
+  sql"insert into referee (fiks_id, name) values($fiksId, $name)".update
