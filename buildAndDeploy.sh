cf6394a871ace3e3cb21cdbb90aa2eef60e5dd6e
Morten Andersen-Gott <morten.andersengott@gmail.com>
2021-12-21 11:53:12 +0100

    Deploy script

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-
+set -e
 if [ "$#" -lt 1 ]
 then
   echo "Missing heroku app name"
   exit 1
 fi
-echo "building"
-sbt stage
-echo "pushing"
+if [[ "$1" = "kortglad" ]]
+then
+  tag="prod"
+elif [[ "$1" = "stage" ]]
+then
+  tag="stage"
+else
+  echo "Invalid app name ($1), must be kortglad or stage"
+fi
+
+git update-index --refresh
+git diff-index --quiet HEAD -- || echo "Uncommitted changes, aborting"; exit 1
+
+echo "Building"
+#sbt stage
+echo "Pushing container to heroku"
 heroku container:push web --app $1
-echo "releasing"
+echo "Releasing"
 heroku container:release web --app $1
+
+echo "Tagging successful release with $tag"
+git push origin :refs/tags/${tag}
+git tag -fa ${tag} -m "Heroku deploy to $1"
+git push origin main ${tag}
+
 heroku logs --app $1
