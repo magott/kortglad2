f79cbe1646f3e20b8470ef392474e2138a237275
Morten Andersen-Gott <morten.andersengott@gmail.com>
2022-09-16 10:14:51 +0200

    Support fly io db

--- src/main/scala/kortglad/Main.scala
+++ src/main/scala/kortglad/Main.scala
@@ -5,19 +5,23 @@ import bloque.pg.Pg
 import bloque.jetty.*
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import org.flywaydb.core.Flyway
+import org.slf4j.LoggerFactory
 
 import java.sql.Types
 import java.sql.ResultSet
-
 import java.time.OffsetDateTime
 import scala.util.Properties
-
 import scala.util.Using
 import java.time.OffsetDateTime
-
 import java.util.TimeZone
+
+val logger = LoggerFactory.getLogger("Main")
+
 @main def main =
   TimeZone.setDefault(TimeZone.getTimeZone("Europe/Oslo"))
+  Properties
+    .envOrNone("FLY_APP_NAME")
+    .foreach(_ => logger.info("Starting as fly.io app"))
   val hikariConfig = Properties
     .envOrNone("DATABASE_URL")
     .map(databaseUrlToHikariConfig)
@@ -44,10 +48,12 @@ import java.util.TimeZone
 
 def databaseUrlToHikariConfig(dbUriString: String) =
   val dbUri = java.net.URI.create(dbUriString)
+  val flyIOAppName = Properties.envOrNone("FLY_APP_NAME")
+  val sslMode = flyIOAppName.map(_ => "").getOrElse("?sslmode=require")
   val username = dbUri.getUserInfo.split(":")(0)
   val password = dbUri.getUserInfo.split(":")(1)
   val jdbcUrl =
-    s"jdbc:postgresql://${dbUri.getHost}:${dbUri.getPort}${dbUri.getPath}?sslmode=require"
+    s"jdbc:postgresql://${dbUri.getHost}:${dbUri.getPort}${dbUri.getPath}${sslMode}"
   val config = HikariConfig()
   config.setJdbcUrl(jdbcUrl)
   config.setUsername(username)
