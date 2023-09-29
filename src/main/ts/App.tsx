6aed0e92968919b4367e1a8ab62b211f0335588a
Morten Andersen-Gott <morten.andersengott@gmail.com>
2023-09-29 09:53:29 +0200

    Henter statistikk ved spinner

--- src/main/ts/App.tsx
+++ src/main/ts/App.tsx
@@ -153,8 +153,7 @@ const App: React.VFC = () => {
             </Col>
             <Col xs={3}>
               <Button variant="primary" onClick={fetchDommer} disabled={!dommer}>
-                {fetching && <Spinner as="span" animation="border" size="sm" />}
-                Hent statistikk
+                {fetching ? <Spinner as="span" animation="border" size="sm" /> && 'Henter statistikk' : 'Hent statistikk'}
               </Button>
             </Col>
           </Row>
